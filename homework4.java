912674f762f538180383a19b60622c32fe603306
creative941 <101525879+creative941@users.noreply.github.com>
2022-03-29 22:40:39 +0300

    Add files via upload

---
+++ homework4.java
@@ -0,0 +1,24 @@
+public class homework4 {
+    public static void main(String[] args) {
+        String myName = "Georgi"; // домашно 2, задача 1
+
+        System.out.println(myName); // домашно 2, задача 1
+
+        String friendName = "Diana"; // домашно 2, задача 2
+
+        System.out.println(myName.equals(friendName)); // домашно 2, задача 2
+        System.out.println(myName.equalsIgnoreCase(friendName)); // домашно 2, задача 2
+        System.out.println(myName.compareTo(friendName)); // домашно 2, задача 2
+
+        System.out.println(myName.concat("Iliev")); // домашно 2, задача 3 // без делиметър
+
+        String myAge = "28"; // домашно 2, задача 4
+        System.out.println(myAge); // домашно 2, задача 4
+
+        System.out.println(myName.concat(" ").concat("Iliev").concat(" ").concat(myAge)); //домашно2,задача5,делим.
+
+        System.out.println(friendName + ("\nRizova") + ("\n27") ); // домашно 2, задача 6
+
+
+    }
+}
